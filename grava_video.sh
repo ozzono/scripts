c931d04243a36da84b71b43df52146773caf8335
ozono <oz@oz.com>
2018-09-30 01:15:36 -0300

    Correção de usuário

--- grava_video.sh
+++ grava_video.sh
@@ -2,6 +2,7 @@
 
 ## 24/03/2018 - adding record duration argument
 now=$(date +%Y-%m-%d_%H:%M);
+usuario=$(whoami);
 
 if [ "$1" -gt "-1" ]
 	then
@@ -11,7 +12,7 @@ if [ "$1" -gt "-1" ]
 		echo $i;
 	done
 	echo $i":Vai!";
-	ffmpeg -video_size 1366x768 -framerate 10 -f x11grab -i :0.0+0,0 -f alsa -ac 2 -i pulse -acodec aac -strict experimental -vf scale=852:-1 -t $1 '/home/administrador/Videos/Records/'$now.mp4
+	ffmpeg -video_size 1366x768 -framerate 10 -f x11grab -i :0.0+0,0 -f alsa -ac 2 -i pulse -acodec aac -strict experimental -vf scale=852:-1 -t $1 '/home/'$usuario'/Videos/Records/'$now.mp4
 else
-	ffmpeg -video_size 1366x768 -framerate 10 -f x11grab -i :0.0+0,0 -f alsa -ac 2 -i pulse -acodec aac -strict experimental -vf scale=852:-1 '/home/administrador/Videos/Records/'$now.mp4
+	ffmpeg -video_size 1366x768 -framerate 10 -f x11grab -i :0.0+0,0 -f alsa -ac 2 -i pulse -acodec aac -strict experimental -vf scale=852:-1 '/home/'$usuario'/Videos/Records/'$now.mp4
 fi
