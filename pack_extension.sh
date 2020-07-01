e987138406284b082f497405c8c3086ac42fc9b5
Hugo Virgílio <huvirgilio@gmail.com>
2020-07-01 15:54:22 -0300

    Removed zip command comment; Added old file deletion if existent

--- pack_extension.sh
+++ pack_extension.sh
@@ -5,6 +5,10 @@ if [ -f "$FILE" ]; then
     CURRENT=$(pwd)
     DIRS=(${CURRENT//\// })
     EXTENSION=${DIRS[${#DIRS[@]} - 1]}
+    OLDFILE="../$EXTENSION.zip"
+    if [ -f "$OLDFILE" ]; then
+        rm $OLDFILE
+    fi
     echo "Packing files to $EXTENSION.zip"
-    # zip -r -FS ../$EXTENSION.zip * --exclude '*.git*'.
+    zip -r -FS ../$EXTENSION.zip * --exclude '*.git*'.
 fi
