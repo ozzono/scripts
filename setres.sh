1ab4c92f8d940af6d08ef22e2bb96c7b69e551a8
Hugo Virgílio <huvirgilio@gmail.com>
2021-04-27 15:54:40 -0300

    Random Commit: Many a writer seems to think he is never profound except when he can't understand his own meaning. 		-- George D. Prentice

--- setres.sh
+++ setres.sh
@@ -4,7 +4,7 @@
 # cvt 1920 1080 60
 # xrandr -q lists available video outputs
 
-if [[ $(xrandr -q|grep 1920x1080_60.00|wc -l) -eq "1" ]]; then
+if [[ $(xrandr -q|grep 1920x1080|grep \* |wc -l) -eq "2" ]]; then
     echo "screen already configured"
     exit 1
 fi
@@ -12,7 +12,7 @@ echo "configuring"
 
 xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
 
-xrandr --addmode DP-1 1920x1080_60.00 # replace DP-! with the desired video output
+xrandr --addmode DP-2 1920x1080_60.00 # replace DP-! with the desired video output
 
-xrandr --output DP-1 --mode 1920x1080_60.00
+xrandr --output DP-2 --mode 1920x1080_60.00
 
