73d0a5c056c7a86137736d98116726269b406dbf
Hugo Virgílio <huvirgilio@gmail.com>
2022-01-11 14:04:51 -0300

    Fortune's Real-Life Courtroom Quote #52:
    Q:  What is your name?
    A:  Ernestine McDowell.
    Q:  And what is your marital status?
    A:  Fair.

--- foxkill.sh
+++ foxkill.sh
@@ -1,9 +1,12 @@
 #/bin/bash
 
-list=$(screen -ls)
-if [[ $list == *"foxkill"* ]]; then
-    echo "foxkills screens is already running"
-else
-    echo "Starting foxkill"
-    screen -dmS foxkill bash -c '/home/hugo/bin/foxkill'
-fi
+# list=$(screen -ls)
+# if [[ $list == *"foxkill"* ]]; then
+#     echo "foxkills screens is already running"
+# else
+#     echo "Starting foxkill"
+#     screen -dmS foxkill bash -c '/home/hugo/bin/foxkill'
+# fi
+
+
+kill $(pidof firefox)
