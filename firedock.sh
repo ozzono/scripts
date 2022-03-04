3b80ebff586fde5f30c14e5e8ffb9bd9c15a2356
Hugo Virgilio <huvirgilio@gmail.com>
2022-03-04 11:56:56 -0300

    Fortune Commit: Abstract: 	This study examined the incidence of neckwear tightness among a group of 94 white-collar working men and the effect of a tight business-shirt collar and tie on the visual performance of 22 male subjects.  Of the white-collar men measured, 67% were found to be wearing neckwear that was tighter than their neck circumference.  The visual discrimination of the 22 subjects was evaluated using a critical flicker frequency (CFF) test.  Results of the CFF test indicated that tight neckwear significantly decreased the visual performance of the subjects and that visual performance did not improve immediately when tight neckwear was removed. 		-- Langan, L. M. and Watkins, S. M. "Pressure of Menswear on the 		   Neck in Relation to Visual Performance."  Human Factors 29, 		   #1 (Feb. 1987), pp. 67-71.

--- firedock.sh
+++ firedock.sh
@@ -1,10 +1,37 @@
-#!/bin/bash
 
-firedock=$(screen -ls)
+firedock(){
+    running_foxes=$(docker ps -a --filter "status=running" |grep firefox|wc -l)
+    all_foxes=$(docker ps -a |grep firefox|wc -l)
+    compare="1"
 
-if [[ $firedock == *"firedock"* ]]; then
-    echo $(date +"%Y-%m-%d %H:%M:%S") "firefox docker already enabled"
-else
-    echo $(date +"%Y-%m-%d %H:%M:%S") "enabling firefox docker refreshment"
-    screen -dmS fresh-corner bash -c 'date +"%Y-%m-%d %H:%M:%S enabling firefox docker"; docker run -it --name firedock --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix --net=host firefox; '
-fi
+    if [ $all_foxes = "1" ]; then
+            echo "firedock: $all_foxes stance(s)"
+        if [ $running_foxes = "1" ]; then
+            echo "firedock: stance is already running"
+        else
+            echo "firedock: restarting stopped firedock"
+            docker start firedock
+        fi
+    else
+        echo "firedock: starting new firedock"
+        docker run -it -e DISPLAY=$DISPLAY --name firedock -v /tmp/.X11-unix/:/tmp/.X11-unix --net=host firefox
+    fi
+}
+
+firedock
+
+    # if [[ "$running" -eq "1"]]; then
+    #     echo "teste"
+    # fi
+
+# firedock=$(screen -ls)
+
+# if [[ $firedock == *"firedock"* ]]; then
+#     echo $(date +"%Y-%m-%d %H:%M:%S") "firefox docker already enabled"
+# else
+#     echo $(date +"%Y-%m-%d %H:%M:%S") "enabling firefox docker refreshment"
+#     screen -dmS fresh-corner bash -c 'date +"%Y-%m-%d %H:%M:%S enabling firefox docker"; docker run -it --name firedock --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix --net=host firefox; '
+# fi
+
+
+
