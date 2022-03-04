246b77a69fd9eacbd65d944f3523855334d5500e
Hugo Virgilio <huvirgilio@gmail.com>
2022-03-04 10:48:03 -0300

    Fortune Commit: Wouldn't this be a great world if being insecure and desperate were a turn-on? 		-- "Broadcast News"

--- firedock.sh
+++ firedock.sh
@@ -6,6 +6,5 @@ if [[ $firedock == *"firedock"* ]]; then
     echo $(date +"%Y-%m-%d %H:%M:%S") "firefox docker already enabled"
 else
     echo $(date +"%Y-%m-%d %H:%M:%S") "enabling firefox docker refreshment"
-    screen -dmS fresh-corner bash -c 'date +"%Y-%m-%d %H:%M:%S enabling firefox docker"; docker run -it --name firedock --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix --net=host firefox'
-    
+    screen -dmS fresh-corner bash -c 'date +"%Y-%m-%d %H:%M:%S enabling firefox docker"; docker run -it --name firedock --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix --net=host firefox; '
 fi
