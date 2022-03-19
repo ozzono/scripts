e405b53fc7294d3d26d7a99c11f54513b9068a30
Hugo Virgílio <huvirgilio@gmail.com>
2022-03-18 23:22:23 -0300

    Fortune Commit: "I have a friend who just got back from the Soviet Union, and told me the people there are hungry for information about the West.  He was asked about many things, but I will give you two examples that are very revealing about life in the Soviet Union.  The first question he was asked was if we had exploding television sets.  You see, they have a problem with the picture tubes on color television sets, and many are exploding.  They assumed we must be having problems with them too.  The other question he was asked often was why the CIA had killed Samantha Smith, the little girl who visited the Soviet Union a few years ago; their propaganda is very effective. -- Victor Belenko, MiG-25 fighter pilot who defected in 1976    "Defense Electronics", Vol 20, No. 6, pg. 100

--- watch-rpi.sh
+++ watch-rpi.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 watchpi(){
     while true ; do 
-    if ping -c 1 raspberrypi.local > /dev/null ; then 
-        date +"%Y-%m-%d %H:%M:%S rpi connected"
-        sleep 60
-    else 
+    rpi=$(sudo nmap -sP -n 192.168.1.0/24|grep Raspberry|wc -l)
+    if [ $rpi = "0" ]; then
+    # if ping -c 1 raspberrypi.local > /dev/null ; then 
         a=$(date +"%Y-%m-%d %H:%M:%S rpi node disconnected")
         notify-send "$a"
         sleep 30
+    else 
+        date +"%Y-%m-%d %H:%M:%S rpi connected"
+        sleep 60
     fi
     done
 }
@@ -18,7 +20,7 @@ if [ -z $1 ]; then
         date +"%Y-%m-%d %H:%M:%S watch_rpinode screen is already running"
     else
         date +"%Y-%m-%d %H:%M:%S starting watch_rpinode"
-        screen -dmS watch_rpinode bash -c "$HOME/scripts/watch-rpi.sh watch"
+        screen -dmS watch_rpinode bash -c "sudo bash $HOME/scripts/watch-rpi.sh watch"
     fi
 else
     watchpi
